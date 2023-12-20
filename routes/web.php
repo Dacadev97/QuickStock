5e9c8829edf846363b7f50335224ea8d462e4237
DakaAlvarez97 <davidson.cadavid@est.iudigital.edu.co>
2023-12-20 11:59:47 -0500

    Refactor routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Middleware;
 use App\Models\Product;
 use App\Http\Controllers\SalesController;
 use App\Models\Sale;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,17 +24,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('login', [AuthController::class, 'login']);
-Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('register', [AuthController::class, 'register']);
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('login', 'showLoginForm')->name('login');
+    Route::post('login', 'login');
+    Route::get('register', 'showRegistrationForm')->name('register');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout')->name('logout');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard')->middleware('auth');
 
-Route::get('/productos', [ProductController::class, 'index'])->name('productos')->middleware('auth');
+
 
 Route::get('/ventas', function () {
     $products = Product::all();
@@ -42,12 +45,12 @@ Route::get('/ventas', function () {
 })->name('ventas')->middleware('auth');
 
 
-Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('auth');
 
-Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware('auth');
-
-Route::post('productos', [ProductController::class, 'store'])->name('productos.store')->middleware('auth');
-
-Route::get('/lista', [ProductController::class, 'showAll'])->name('lista')->middleware('auth');
-
-Route::post('/products/{id}/sell', [ProductController::class, 'sell'])->name('sell')->middleware('auth');
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/productos', 'index')->name('productos')->middleware('auth');
+    Route::delete('products/{product}', 'destroy')->name('products.destroy')->middleware('auth');
+    Route::put('products/{product}', 'update')->name('products.update')->middleware('auth');
+    Route::post('productos', 'store')->name('productos.store')->middleware('auth');
+    Route::get('/lista', 'showAll')->name('lista')->middleware('auth');
+    Route::post('/products/{id}/sell', 'sell')->name('sell')->middleware('auth');
+});
